2df5d12aa86ad8f6568e2e2e95b52f59820148b8
Chobe <42force@gmail.com>
2019-05-17 20:28:46 +0800

    lessons day 2

---
+++ lesson6elseif.py
@@ -0,0 +1,84 @@
+#Day 2 
+# true or false and booleans 
+#Comparisons 
+
+#Equal                  : ==
+#Not Equal              : !=
+#Greater Than           : >
+#Less Than              : <
+# Greater than or Equal : >=
+# Less or Equal         : <=
+# Object identity       : is - if values have the same id - or same object 
+
+# boolean
+#==============================#
+# and 
+# or 
+# not
+
+#comparison example 
+#==============================#
+# language = 'JavaScript'
+# if language == 'Python':
+#     print('Language is I love you')
+# elif language == 'JavaScript':
+#     print('Language is Javascript')
+# elif language == 'Java':
+#     print('Language is Java')
+# else:
+#     print('Excuse my French')
+
+# boolean examples 
+#=================================#
+# user = 'Admin'
+# logged_in = False  #true or false 
+
+# if user == 'Admin' or logged_in:
+#     print('You are on the auth page')
+# else:
+#     print('Not Authorised')
+
+# if not logged_in:
+#     print('Please log in')
+# else: 
+#     print('Auth page')
+
+
+# a = [1, 2, 3]
+# #b = [1, 2, 3]
+
+# b = a
+
+# print (id(a)) #checking the object
+# print (id(b)) 
+# print( a is b)
+
+
+# False Values Lesson :
+    # False
+    # None
+    # Zero of any numeric type
+    # Any empty sequence. e.g. '', (), [].
+    # Any empty mapping e.g. {},
+
+condition = 'I love you'
+
+if condition:
+    print('Evaluated to True')
+else: 
+    print('Evaluated to False')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
